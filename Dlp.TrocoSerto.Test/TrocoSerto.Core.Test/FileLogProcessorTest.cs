5136e51cf7cc36da24deeb0460823058e609a1a3
eduardojonssen <eduardojonssen@gmail.com>
2015-09-30 20:05:00 -0300

    Adicionado IocFactory.

---
+++ Dlp.TrocoSerto.Test/TrocoSerto.Core.Test/FileLogProcessorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrocoSerto.Core.Log;
+using Dlp.TrocoSerto.Test.TrocoSerto.Core.Test.Mocks;
+using Dlp.Framework.Container;
+using TrocoSerto.Core.Utility;
+
+namespace Dlp.TrocoSerto.Test.TrocoSerto.Core.Test {
+	[TestClass]
+	public class FileLogProcessorTest {
+
+		[TestMethod]
+		public void Save_WriteBananaToFile_Test() {
+
+			LogData logData = new LogData();
+			logData.Category = "Fruta";
+			logData.LogType = "Erro";
+			logData.ObjectToLog = "Banana";
+			logData.MethodName = "Save_WriteBanana_To_File";
+
+			ConfigurationUtilityMock mock = new ConfigurationUtilityMock();
+			mock.LogPath = "c:/BananaDePijama";
+			mock.LogType = "File";
+
+			IocFactory.Register(
+					Component.For<IConfigurationUtility>()
+					.Instance(mock),
+
+					Component.For<ILog>()
+					.ImplementedBy<FileLogProcessor>()
+					.Interceptor<LogInterceptor>()
+				);
+
+
+
+			ILog log = IocFactory.Resolve<ILog>();
+
+			log.Save(logData);
+		}
+	}
+}
---
+++ TrocoSerto.Core/Log/LogInterceptor.cs
@@ -0,0 +1,24 @@
+﻿using Dlp.Framework.Container.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrocoSerto.Core.Log {
+	
+	public class LogInterceptor : IInterceptor {
+
+		public void Intercept(IInvocation invocation) {
+
+			// Verifica se o metodo chamado é o metodo Save.
+			if (invocation.MethodInvocationTarget.Name.Equals("Save") == true) {
+
+				LogData logData = invocation.Arguments[0] as LogData;
+				logData.Category += "_banana";
+			}
+
+			invocation.Proceed();
+		}
+	}
+}
